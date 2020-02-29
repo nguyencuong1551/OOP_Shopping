02d58eeb9e96670eeab2d2c3dee1e6f2ab31726d
nguyencuong1551 <nguyencuong1551070711@gmail.com>
2020-02-29 18:25:41 +0700

    Login && Checkadmin

--- website Shopping OOP/Customer/header.php
+++ website Shopping OOP/Customer/header.php
@@ -54,11 +54,40 @@ $data = new databaseShopping();
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">Company name</a>
     <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-    <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">Login</a>
-        </li>
-    </ul>
+
+        <?php
+        $getUser_name = $_COOKIE['nameUser'];
+        $getUser_id = $_COOKIE['idUser'];
+        $getUser_role = $_COOKIE['roleUser'];
+        if(isset($getUser_id))
+        {
+            if($getUser_role == 'admin')  // hàm isset chỉ là hàm check tồm tại chứ không phải biến;
+            {
+                echo "
+             <ul class=\"navbar-nav px-3\">
+            <li class=\"nav-item text-nowrap\">
+                        <a class=\"nav-link\" href=\"../Admin/admin.php\">$getUser_name</a>
+                    </li>
+                    </ul>
+                     <ul class=\"navbar-nav px-3\">
+                    <li class=\"nav-item text-nowrap\">
+                        <a class=\"nav-link\" href=\"logout.php\">Logout</a>
+                    </li></ul>
+                    ";
+            }else{
+                echo "
+                 <ul class=\"navbar-nav px-3\">
+                <li class=\"nav-item text-nowrap\">
+                            <a class=\"nav-link\" href=\"logout.php\">Logout</a>
+                        </li></ul>";
+            }
+        }else {
+            echo " <ul class=\"navbar-nav px-3\"><li class=\"nav-item text-nowrap\">
+            <a class=\"nav-link\" href=\"login.php\">Login</a>
+        </li> </ul>";
+        }
+        ?>
+
 </nav>
 
 <div class="container-fluid">
